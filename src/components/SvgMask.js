e17fb8b138a38eb19adad51eeb2b2f6d3215eb9f
Sagar Khatri <sagark1510@gmail.com>
2019-02-05 16:13:59 +0530

    path props utilized after animation complete

--- src/components/SvgMask.js
+++ src/components/SvgMask.js
@@ -1,19 +1,19 @@
 // @flow
-import React, { Component } from 'react';
-import {
-  View,
-  Animated,
-  Easing,
-  Dimensions,
-} from 'react-native';
+import React, {Component} from 'react';
+import {View, Animated, Easing, Dimensions} from 'react-native';
 // import { Svg } from 'expo';
 import Svg from 'react-native-svg';
 import AnimatedSvgPath from './AnimatedPath';
 
-import type { valueXY } from '../types';
+import type {valueXY} from '../types';
 
 const windowDimensions = Dimensions.get('window');
-const path = (size, position, canvasSize): string => `M0,0H${canvasSize.x}V${canvasSize.y}H0V0ZM${position.x._value},${position.y._value}H${position.x._value + size.x._value}V${position.y._value + size.y._value}H${position.x._value}V${position.y._value}Z`;
+const path = (size, position, canvasSize): string =>
+  `M0,0H${canvasSize.x}V${canvasSize.y}H0V0ZM${position.x._value},${
+    position.y._value
+  }H${position.x._value + size.x._value}V${position.y._value + size.y._value}H${
+    position.x._value
+  }V${position.y._value}Z`;
 
 type Props = {
   size: valueXY,
@@ -53,19 +53,30 @@ class SvgMask extends Component<Props, State> {
   }
 
   componentWillReceiveProps(nextProps) {
-    if (this.props.position !== nextProps.position || this.props.size !== nextProps.size) {
+    if (
+      this.props.position !== nextProps.position ||
+      this.props.size !== nextProps.size
+    ) {
       this.animate(nextProps.size, nextProps.position);
     }
   }
 
   animationListener = (): void => {
-    const d: string = path(this.state.size, this.state.position, this.state.canvasSize);
+    const pathFn = this.props.path || path;
+    const d: string = pathFn(
+      this.state.size,
+      this.state.position,
+      this.state.canvasSize,
+    );
     if (this.mask) {
-      this.mask.setNativeProps({ d });
+      this.mask.setNativeProps({d});
     }
   };
 
-  animate = (size: valueXY = this.props.size, position: valueXY = this.props.position): void => {
+  animate = (
+    size: valueXY = this.props.size,
+    position: valueXY = this.props.position,
+  ): void => {
     if (this.props.animated) {
       Animated.parallel([
         Animated.timing(this.state.size, {
@@ -83,36 +94,50 @@ class SvgMask extends Component<Props, State> {
       this.state.size.setValue(size);
       this.state.position.setValue(position);
     }
-  }
+  };
 
-  handleLayout = ({ nativeEvent: { layout: { width, height } } }) => {
+  handleLayout = ({
+    nativeEvent: {
+      layout: {width, height},
+    },
+  }) => {
     this.setState({
       canvasSize: {
         x: width,
         y: height,
       },
     });
-  }
+  };
 
   render() {
     const pathFn = this.props.path || path;
     return (
-      <View pointerEvents="box-none" style={this.props.style} onLayout={this.handleLayout}>
-        {
-          this.state.canvasSize
-            ? (
-              <Svg pointerEvents="none" width={this.state.canvasSize.x} height={this.state.canvasSize.y}>
-                <AnimatedSvgPath
-                  ref={(ref) => { this.mask = ref; }}
-                  fill={this.props.backdropColor}
-                  fillRule="evenodd"
-                  strokeWidth={1}
-                  d={pathFn(this.state.size, this.state.position, this.state.canvasSize)}
-                />
-              </Svg>
-            )
-            : null
-        }
+      <View
+        pointerEvents="box-none"
+        style={this.props.style}
+        onLayout={this.handleLayout}
+      >
+        {this.state.canvasSize ? (
+          <Svg
+            pointerEvents="none"
+            width={this.state.canvasSize.x}
+            height={this.state.canvasSize.y}
+          >
+            <AnimatedSvgPath
+              ref={ref => {
+                this.mask = ref;
+              }}
+              fill={this.props.backdropColor}
+              fillRule="evenodd"
+              strokeWidth={1}
+              d={pathFn(
+                this.state.size,
+                this.state.position,
+                this.state.canvasSize,
+              )}
+            />
+          </Svg>
+        ) : null}
       </View>
     );
   }
